42d284810c3c35234c8384d0943a4513af779b10
a1105013796 <32124539+a1105013796@users.noreply.github.com>
2020-03-17 19:55:36 +0800

    Revert "1、新增容错测试"

--- provider-demo/src/main/java/cn/cwbolg/providerdemo/controler/ProciderController.java
+++ provider-demo/src/main/java/cn/cwbolg/providerdemo/controler/ProciderController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 /**
+ *
+ *
  * @description: 服务提供者
  * @author: ChenWei
  * @create: 2020/3/11 - 20:08
@@ -11,23 +13,12 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api")
 public class ProciderController {
-	int i = 0;
 
 	@Value("${server.port}")
 	String port;
 
 	@GetMapping("/echo/{message}")
-	String echo(@PathVariable("message") String message) {
-
-		return "Hello Nacos " + message + ", i am  " + port;
-	}
-
-	@GetMapping("/echo2")
-	String echo2() {
-		i++;
-		if(i%2==0){
-			throw new RuntimeException("异常");
-		}
-		return "Hello Nacos " ;
+	String echo(@PathVariable("message") String message){
+		return "Hello Nacos " + message+", i am  "+port;
 	}
 }
